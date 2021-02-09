718b141cf9544c1ff52477b6dfebdca567cf48e6
Alvin Seville <alvinseville7cf@gmail.com>
2021-02-09 14:24:19 +0000

    Code simplified

--- main.sh
+++ main.sh
@@ -1,25 +1,16 @@
-list=("x" "x1" "1x"
-  "x = 0" "x1 = 0" "1x = 0"
-  "var x = 0" "var x1 = 0" "var 1x = 0"
-  "int x = 0" "int x1 = 0" "int 1x = 0")
+list=("var x = 0" "var x = 22.5")
 
 integerNumberGlob='?(-|+)+([[:digit:]])?(u|ul|lu)'
 floatingNumberGlob='?(-|+)+([[:digit:]])?(.+([[:digit:]]))?(d|f|m)'
 charGlob="'?'"
 stringGlob='"*"'
-valueGlob="@($integerNumberGlob|$floatNumberGlob|$charGlob|$stringGlob)"
-
-integralTypeGlob='sbyte|byte|short|ushort|int|uint|long|ulong'
-floatingPointTypeGlob='float|double|decimal'
-charTypeGlob='char'
-stringTypeGlob='string'
-varTypeGlob='var'
-typeGlob="@($integralTypeGlob|$floatingPointTypeGlob|$charTypeGlob|$stringTypeGlob|$varTypeGlob)"
+nullGlob='null'
+valueGlob="@($integerNumberGlob|$floatingNumberGlob|$charGlob|$stringGlob|$nullGlob)"
 
 identifierGlob='[[:alpha:]]*([[:alnum:]])'
+typeGlob="$identifierGlob"
 
 glob="$typeGlob+([[:space:]])$identifierGlob*([[:space:]])=*([[:space:]])$valueGlob"
-echo "glob == $glob"
 
 shopt -s nocasematch
 for i in "${list[@]}"
